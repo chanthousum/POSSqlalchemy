c97f56b1d08b31a6f53723466f2905bac9d56eca
chanthousum <35910993+chanthousum@users.noreply.github.com>
2019-07-30 10:47:43 +0700

    fixed

--- POSSqlalchemy/index.py
+++ POSSqlalchemy/index.py
@@ -1,50 +1,67 @@
+
 from flask import Flask,render_template,request,session,flash,redirect,url_for
-from flask_sqlalchemy import SQLAlchemy,Model
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+# from POSSqlalchemy.Classes.Users import *
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1@localhost/sqlalchemy'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1@localhost/sqlalchemy'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:chanthou123@localhost:5432/sqlalchemy'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 # app.config['SQLALCHEMY_BINDS']= {
 #     'users':        'mysql+pymysql://root:1@localhost/sqlalchemy',
 #  }#delete table and other
 app.config['SECRET_KEY'] = "random string"
 db=SQLAlchemy(app)
-
-
-
-class products(db.Model):
-    producutid = db.Column('producutid', db.Integer, primary_key=True)
-    productname = db.Column(db.String(100))
-    categoryid = db.Column(db.Integer, db.ForeignKey('products.categoryid'), nullable=False)#one to many
-    def __init__(self, productname):
-        self.productname = productname
-
-class Categorys(db.Model):
-    categoryid = db.Column('categoryid', db.Integer, primary_key=True)
-    catetoryname = db.Column(db.String(100))
-
+# class products(db.Model):
+#     producutid = db.Column('producutid', db.Integer, primary_key=True)
+#     productname = db.Column(db.String(100))
+#     categoryid = db.Column(db.Integer, db.ForeignKey('products.categoryid'), nullable=False)#one to many
+#     def __init__(self, productname):
+#         self.productname = productname
+# class Categorys(db.Model):
+#     categoryid = db.Column('categoryid', db.Integer, primary_key=True)
+#     catetoryname = db.Column(db.String(100))
 class Students(db.Model):
+    __tablename__ = 'tblstudent'
     id = db.Column('student_id', db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     city = db.Column(db.String(50))
     addr = db.Column(db.String(200))
     pin = db.Column(db.String(10))
+    created_at=db.Column(db.DateTime,default=datetime.utcnow,onupdate=datetime.utcnow)
+    updated_at=db.Column(db.DateTime)
     def __init__(self,name,city,addr,pin):
         self.name=name
         self.city=city
         self.addr=addr
         self.pin=pin
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @app.route('/')
 def Index():
     db.create_all()
     # db.create_all(bind=['users'])
     return render_template("home.html")
-
 @app.route('/student')
 def StudentList():
     objstudent=Students.query.order_by(Students.name).limit(10).all()
@@ -55,8 +72,6 @@ def DeleteUserid(id):
     db.session.delete(objstudent)
     db.session.commit();
     return redirect(url_for("StudentList"))
-
-
 @app.route('/addstudent',methods = ['GET', 'POST'])
 def AddStudent():
     if request.method == 'POST':
@@ -72,16 +87,13 @@ def AddStudent():
             db.session.add(objstudent)
             db.session.commit()
     return redirect(url_for("StudentList"))
-
-
-
-# @app.route('/search/',methods = ['GET', 'POST'])
-# def UserSearch():
-#     if request.method == 'POST':
-#         o = students("", "", "", "")
-#         o.name = request.form['txt_search'];
-#         objstudent=students.query.filter_by(name=name).first()
-#     return render_template("student.html",student=objstudent)
+@app.route('/search',methods = ['GET', 'POST'])
+def UserSearch():
+    if request.method == 'POST':
+        objstudent=Students("", "", "", "")
+        objstudent.name = request.form['txt_search'];
+        objstudent1=Students.query.filter_by(name=objstudent.name).all()
+    return render_template("student.html",student=objstudent1)
 
 
 
@@ -102,3 +114,6 @@ if __name__ == '__main__':
 
     # https: // virtualzero.net / blog / connect - a - flask - app - to - a - mysql - database -
     # with-sqlalchemy - and -pymysql
+
+    # https: // realpython.com / flask - by - example - part - 2 - postgres - sqlalchemy - and -alembic /
+    # https: // realpython.com / flask - connexion - rest - api - part - 2 /
