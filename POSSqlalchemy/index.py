1209e3ecf1639232451f02d1d7fa1671036689c8
chanthousum <35910993+chanthousum@users.noreply.github.com>
2019-08-13 14:13:38 +0700

    update user

--- POSSqlalchemy/index.py
+++ POSSqlalchemy/index.py
@@ -42,65 +42,77 @@ class Positions(db.Model):
     updated_at = db.Column(db.DateTime)
 @app.route('/login',methods=['POST'])
 def Login():
+    return render_template("login_frm.html")
     error=None
     if request.method=="POST":
-        objUser1=Users()
-        objUser1.UserName=request.form['txt_username']
-        objUser1.Password = request.form['txt_password']
-        objUser=Users.query.filter_by(UserName=objUser1.UserName,Password=objUser1.Password).first()
-        if objUser in None:
-            error = "Username and Password is invalid"
-            return render_template("login_frm.html", error=error)
-        else:
-            return render_template("index.html")
-
-@app.route('/')
+        # objUser1=Users()
+        # objUser1.UserName=request.form['txt_username']
+        # objUser1.Password = request.form['txt_password']
+        objUser = Users.query.all()
+        for col in objUser:
+            print(col.UserName)
+        return render_template("login_frm.html",userlists=objUser)
+app.add_url_rule("/","login",Login)
+@app.route('/home')
 def Index():
     # db.create_all()
     return render_template("index.html")
-@app.route('/userlist')
+@app.route('/userlist',methods=['GET'])
 def UserList():
-
-    return render_template("users/userlist.html",userlist=userlist)
-
-@app.route('/AddUser',methods = ['POST'])
+    userlist=Users.query.order_by(Users.UserName).all()
+    return render_template("users/userlist.html",userlists=userlist)
+@app.route('/adduser')
 def AddUser():
-    if request.method == 'POST':
-        objUser = Users()
-        objUser.UserName = request.json['UserName']
-        objUser.Gender=request.json['Gender']
-        objUser.PositionID = request.json['PositionID']
+    position=Positions.query.all()
+    return render_template("users/adduser_frm.html",positions=position)
+@app.route('/usercreate',methods=['POST'])
+def Save():
+    try:
+        if request.method == "POST":
+            objUser = Users()
+            objUser.UserName= request.form['txt_username']
+            objUser.Gender = request.form['ddlgender']
+            objUser.DateOfBirth = request.form['txt_date']
+            objUser.Password = request.form['txt_password']
+            objUser.Description = request.form['txt_description']
+            objUser.Salary = request.form['txt_salary']
+            objUser.Phone = request.form['txt_phone']
+            objUser.Active = request.form['ddlactive']
+            objUser.PositionID = request.form['ddlposition']
+            db.session.add(objUser)
+            db.session.commit()
+            flash("Created User")
+        return redirect(url_for("AddUser"))
+    except Exception as ex:
+        return render_template("Error.html",errre_message=ex)
+@app.route('/userupdate/<id>',methods=['POST'])
+def UserUpdate(id):
+    if request.method=="POST":
+        objUser=Users.query.filter_by(UserID=id).first()#get one record
+        objUser.UserName=request.form['txt_username']
+        objUser.Gender = request.form['ddlgender']
+        objUser.DateOfBirth = request.form['txt_date']
+        objUser.Password = request.form['txt_password']
+        objUser.Description = request.form['txt_description']
+        objUser.Salary = request.form['txt_salary']
+        objUser.Phone = request.form['txt_phone']
+        objUser.Active = request.form['ddlactive']
+        objUser.PositionID = request.form['ddlposition']
         db.session.add(objUser)
         db.session.commit()
-        msg_json = {'message': 'User Created'}
-    return jsonify(msg_json)
-@app.route('/GetUserAll',methods=['GET'])
-def GetUserAll():
-    # objUser=Users.query.order_by(Users.UserName).limit(10).all()
-    if request.method=="GET":
-        objUser = Users.query.all()
-        output=[]
-    #     for col in objUser:
-    #         dict={}
-    #         dict['UserID']=col.UserID
-    #         dict['UserName'] = col.UserName
-    #         output.append(dict)
-    # return jsonify(output)
-        return objUser.json()
-
-@app.route('/GetByone/<id>',methods=['GET'])
-def GetByone(id):
-    if request.method=="GET":
-        objUser=Users.query.filter_by(UserID=id).all()
-        output=[]
-        for col in objUser:
-            dict={}
-            dict['UserID']=col.UserID
-            dict['UserName'] = col.UserName
-            dict['Gender'] = col.Gender
-            output.append(dict)
-    return jsonify(output)
-
+        flash("Updated User")
+    return redirect("/useredit/"+id+"")
+@app.route('/useredit/<id>',methods=['GET'])
+def UserEdit(id):
+    userlist=Users.query.filter_by(UserID=id).first()
+    position = Positions.query.all()
+    return render_template("users/edituser_frm.html",userlists=userlist,positions=position)
+@app.route('/userdelete/<id>',methods=['GET'])
+def UserDelete(id):
+    objUser=Users.query.filter_by(UserID=id).first()
+    db.session.delete(objUser)
+    db.session.commit()
+    return redirect(url_for("UserList"))
 @app.route('/AddPosition',methods=['POST'])
 def AddPosition():
     if request.method == 'POST':
@@ -110,12 +122,14 @@ def AddPosition():
         db.session.commit()
         msg_json={'message':'Position Created'}
     return jsonify(msg_json)
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"),404
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True,host="localhost",port=4000)
+    app.run(debug=False,host="localhost",port=4000)
 
     # https: // flask - sqlalchemy.palletsprojects.com / en / 2.
     # x / queries /
